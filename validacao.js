3fee0be012deb026adc3048ae8350522a74492e5
Juliana <87387516+julianahkolmar@users.noreply.github.com>
2022-06-30 17:06:31 -0300

    visualizaçao da foto de perfil

--- validacao.js
+++ validacao.js
@@ -96,10 +96,10 @@ function mascaraTel(i) {
 }
 
 function lerImg() {
-    if(this.files && this.files[0]) {
+    if (this.files && this.files[0]) {
         var file = new FileReader();
-        file.onload = function(e) {
-            document.getElementById("preview") .src = e.target.result;
+        file.onload = function (e) {
+            document.getElementById("preview").src = e.target.result;
         }
         file.readAsDataURL(this.files[0]);
     }
